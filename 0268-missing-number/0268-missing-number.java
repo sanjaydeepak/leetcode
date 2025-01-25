6af609578c3bb2090de33451de28060731421035
sanjaykumar <108710370+sanjaydeepak@users.noreply.github.com>
2025-01-25 22:25:54 +0530

    Time: 1 ms (36.19%), Space: 45.2 MB (69.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,22 +4,24 @@ class Solution {
         while(i<arr.length)
         {
             int correct=arr[i];
-            if(arr[i]<arr.length && arr[i]!=arr[correct])
+            if(arr[i]<arr.length &&arr[i]!=arr[correct])
             {
                 swap(arr,i,correct);
-            }else
+            }
+            else
             {
                 i++;
             }
-           
+            
         }
-        //For Missing Number
-         for(int index=0;index<arr.length;index++)
+        for(int  index=0;index<arr.length;index++)
+            
             {
                 if(arr[index]!=index)
-                return index;
+                {
+                    return index;
+                }
             }
-            // For case 2 Where the the given number has noindex or out of  boounds
         return arr.length;
     }
     void swap(int []arr,int first,int second)
