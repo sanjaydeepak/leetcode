6060c23e5e0e62ceaf72e8b83d23fe37b000863f
sanjaykumar <108710370+sanjaydeepak@users.noreply.github.com>
2025-01-27 20:15:54 +0530

    Time: 6 ms (82.16%), Space: 56.5 MB (72.43%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,36 +1,35 @@
 class Solution {
-    public List<Integer> findDisappearedNumbers(int[] arr) {
-
-        //CYCLIC SORT APPROACH
-        //By 0 To N Index Which is arr.length
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List <Integer>ans=new ArrayList<>();
         int i=0;
-        //Use of Interface
-        List<Integer>ans=new ArrayList<>();
-        while(i<arr.length)
+        while(i<nums.length)
         {
-            int correct_index=arr[i]-1;
-            if(arr[i]!=arr[correct_index])
+            int correct=nums[i]-1;
+            if( nums[i]!=nums[correct])
             {
-                swap(arr,i,correct_index);
+                    swap(nums,i,correct);
+
             }
             else
             {
                 i++;
             }
         }
-        for(int index=0;index<arr.length;index++)
+        for(int index=0;index<nums.length;index++)
         {
-            if(arr[index]!=index+1)
+            if(nums[index]!=index+1)
             {
                 ans.add(index+1);
             }
+
         }
         return ans;
     }
-    void swap(int arr[],int first,int second)
-    {
-        int temp=arr[first];
-        arr[first]=arr[second];
-        arr[second]=temp;
-    }
+        void swap(int []nums,int first, int second)
+        {
+            int temp=nums[first];
+            nums[first]=nums[second];
+            nums[second]=temp;
+        }
+    
 }
